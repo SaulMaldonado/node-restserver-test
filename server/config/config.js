4c7561f5dbb43128a4d338fe081484dd74909768
saul.maos@gmail.com <ZarahyTrezbg>
2018-09-20 01:19:16 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 /// ===============
 
 process.env.PORT = process.env.PORT || 3000;
+
+/// ===============
+/// entorno
+/// ===============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+/// ===============
+/// base de datos
+/// ===============
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:ZarahyTrezmb3+cafe-user@ds163822.mlab.com:63822/cafe';
+}
+
+process.env.URLDB = urlDB;
