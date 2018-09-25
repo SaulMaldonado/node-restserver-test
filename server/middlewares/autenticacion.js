12e9f9a2605ea2cb13df66f2b9e2f33ba8abbbb6
saul.maos@gmail.com <ZarahyTrezbg>
2018-09-25 02:14:16 -0500

    Subida de images

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -17,9 +17,6 @@ let verificaToken = (req, res, next) => {
         }
 
         req.usuario = decoded.usuario;
-
-        console.log(decoded.usuario);
-        console.log(req.usuario);
         next();
     })
 
@@ -48,7 +45,32 @@ let verificaAdminRole = (req, res, next) => {
 
 };
 
+// ===========
+// verificar 
+// =======
+
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'token no valido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    })
+
+}
+
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
